8d7f2ba53b71e3d433fb0ead2207d1fdde9810a7
salvage0707 <34135364+salvage0707@users.noreply.github.com>
2025-02-21 01:10:37 +0900

    remove type-check

--- packages/notion-md-converter-types/rollup.config.mjs
+++ packages/notion-md-converter-types/rollup.config.mjs
@@ -1,5 +1,56 @@
-import { createBaseConfig } from "@notion-md-converter/config/rollup";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
 import { defineConfig } from "rollup";
+import dts from "rollup-plugin-dts";
+import typescript from "rollup-plugin-typescript2";
+
 import pkg from "./package.json" assert { type: "json" };
 
-export default defineConfig(createBaseConfig(pkg));
+const external = [
+  ...Object.keys(pkg.dependencies || {}),
+  ...Object.keys(pkg.peerDependencies || {}),
+];
+export default defineConfig([
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: "dist/index.cjs",
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: "dist/index.mjs",
+        format: "es",
+        sourcemap: true,
+      },
+    ],
+    external,
+    plugins: [
+      json({
+        compact: true,
+      }),
+      nodeResolve(),
+      commonjs(),
+      typescript({
+        tsconfig: "./tsconfig.json",
+        exclude: ["src/**/*.test.ts", "src/**/*.spec.ts"],
+      }),
+      terser(),
+    ],
+  },
+  {
+    input: "src/index.ts",
+    output: [{ file: "dist/index.d.ts", format: "es" }],
+    plugins: [
+      dts({
+        tsconfig: "./tsconfig.json",
+        compilerOptions: {
+          composite: false,
+        },
+      }),
+    ],
+  },
+]);
