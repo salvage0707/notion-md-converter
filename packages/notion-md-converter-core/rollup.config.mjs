b35f04e781ae235ad93940fdd63afc230cbb2f2e
salvage0707 <34135364+salvage0707@users.noreply.github.com>
2025-02-21 10:22:09 +0900

    fix build

--- packages/notion-md-converter-core/rollup.config.mjs
+++ packages/notion-md-converter-core/rollup.config.mjs
@@ -1,11 +1,43 @@
-import { createBaseConfig, createConfig } from "@notion-md-converter/config/rollup";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
 import { defineConfig } from "rollup";
+import typescript from "rollup-plugin-typescript2";
+
 import pkg from "./package.json" assert { type: "json" };
 
-const baseConfig = createBaseConfig(pkg);
-const subpathConfigs = [
-  createConfig(pkg, "src/types/index.ts", "dist/types/index.js"),
-  createConfig(pkg, "src/test-helper/index.ts", "dist/test-helper/index.js"),
+const external = [
+  ...Object.keys(pkg.dependencies || {}),
+  ...Object.keys(pkg.peerDependencies || {}),
 ];
-
-export default defineConfig([...baseConfig, ...subpathConfigs]);
+export default defineConfig([
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: "dist/index.cjs",
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: "dist/index.mjs",
+        format: "es",
+        sourcemap: true,
+      },
+    ],
+    // external,
+    plugins: [
+      json({
+        compact: true,
+      }),
+      nodeResolve(),
+      commonjs(),
+      typescript({
+        tsconfig: "./tsconfig.json",
+        exclude: ["src/**/*.test.ts", "src/**/*.spec.ts"],
+      }),
+      terser(),
+    ],
+  },
+]);
