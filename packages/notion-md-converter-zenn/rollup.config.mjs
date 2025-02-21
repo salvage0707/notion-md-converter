28ea3b69cb2ea846df362ecda06da0bf2e7debb5
salvage0707 <34135364+salvage0707@users.noreply.github.com>
2025-02-21 21:05:27 +0900

    catalogを追加

--- packages/notion-md-converter-zenn/rollup.config.mjs
+++ packages/notion-md-converter-zenn/rollup.config.mjs
@@ -1,5 +1,36 @@
-import { createBaseConfig } from "@notion-md-converter/config/rollup";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
 import { defineConfig } from "rollup";
-import pkg from "./package.json" assert { type: "json" };
+import typescript from "rollup-plugin-typescript2";
 
-export default defineConfig(createBaseConfig(pkg));
+export default defineConfig([
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: "dist/index.cjs",
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: "dist/index.mjs",
+        format: "es",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      json({
+        compact: true,
+      }),
+      nodeResolve(),
+      commonjs(),
+      typescript({
+        tsconfig: "./tsconfig.json",
+        exclude: ["src/**/*.test.ts", "src/**/*.spec.ts"],
+      }),
+      terser(),
+    ],
+  },
+]);
