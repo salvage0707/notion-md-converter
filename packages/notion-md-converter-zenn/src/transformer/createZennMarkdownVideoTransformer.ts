b997688ee83bea0dce40ce5468d7257b64de7fc3
salvage0707 <34135364+salvage0707@users.noreply.github.com>
2025-03-02 17:02:42 +0900

    [zenn] youtubeに対応

--- packages/notion-md-converter-zenn/src/transformer/createZennMarkdownVideoTransformer.ts
+++ packages/notion-md-converter-zenn/src/transformer/createZennMarkdownVideoTransformer.ts
@@ -4,6 +4,7 @@ import {
   createNoChangeFileObjectAdapter,
 } from "@notion-md-converter/core";
 import type { FileAdapter, VideoTransformer } from "@notion-md-converter/core/types";
+import { ZennMarkdownUtils } from "../utils";
 
 export const createZennMarkdownVideoTransformer = (
   options: {
@@ -11,8 +12,26 @@ export const createZennMarkdownVideoTransformer = (
   } = {},
 ): VideoTransformer => {
   return createBasicVideoTransformer(({ block }) => {
+    const checkURL = (url: string) => {
+      try {
+        new URL(url);
+        return true;
+      } catch (error) {
+        return false;
+      }
+    };
     const fileAdapter = options.fileAdapter ?? createNoChangeFileObjectAdapter();
     const { url } = fileAdapter(block.video);
+    const isURL = checkURL(url);
+
+    if (isURL) {
+      const urlObj = new URL(url);
+      const domain = urlObj.hostname;
+      if (domain === "www.youtube.com") {
+        return ZennMarkdownUtils.embedYoutube(url);
+      }
+    }
+
     return MarkdownUtils.wrapWithNewLines(MarkdownUtils.video(url));
   });
 };
