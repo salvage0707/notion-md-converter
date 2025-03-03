c988bdb78fcac2b73c88ab8a77ce71c35e14b229
salvage0707 <34135364+salvage0707@users.noreply.github.com>
2025-03-03 23:24:40 +0900

    add qiita breadcrumb

--- packages/notion-md-converter-qiita/src/converter/NotionQiitaMarkdownConverter.ts
+++ packages/notion-md-converter-qiita/src/converter/NotionQiitaMarkdownConverter.ts
@@ -1,11 +1,15 @@
 import {
+  createUnsupportedBlockTransformer,
   NotionMarkdownConverter,
 } from "@notion-md-converter/core";
 import type { TransformerMapping } from "@notion-md-converter/types";
+import { createQiitaMarkdownBookmarkTransformer } from "../transformer/createQiitaMarkdownBookmarkTransformer";
 
 export class NotionQiitaMarkdownConverter extends NotionMarkdownConverter {
   constructor(transformers: TransformerMapping = {}) {
     super({
+      bookmark: createQiitaMarkdownBookmarkTransformer(),
+      breadcrumb: createUnsupportedBlockTransformer(),
       ...transformers,
     });
   }
