ba48ffb9c2d99a8b46e94b8a807ab36e51f63945
salvage0707 <34135364+salvage0707@users.noreply.github.com>
2025-03-06 01:18:17 +0900

    fix providers

--- packages/notion-md-converter-core/src/utils/utils.ts
+++ packages/notion-md-converter-core/src/utils/utils.ts
@@ -21,29 +21,44 @@ export const getProvider = (url: string) => {
   if (["codepen.io"].includes(domain)) {
     return "codepen";
   }
+  if (["github.com"].includes(domain)) {
+    return "github";
+  }
   if (["gist.github.com"].includes(domain)) {
     return "github-gist";
   }
   if (["asciinema.org"].includes(domain)) {
     return "asciinema";
   }
-  if (["www.figma.com"].includes(domain)) {
+  if (["www.figma.com", "figma.com"].includes(domain)) {
     return "figma";
   }
   if (["speakerdeck.com"].includes(domain)) {
     return "speakerdeck";
   }
-  if (["www.slideshare.net"].includes(domain)) {
+  if (["www.slideshare.net", "slideshare.net"].includes(domain)) {
     return "slideshare";
   }
   if (["docs.google.com"].includes(domain) && path.startsWith("/presentation/d/")) {
     return "google-slide";
   }
-  if (["www.docswell.com"].includes(domain)) {
+  if (["www.docswell.com", "docswell.com"].includes(domain)) {
     return "docswell";
   }
-  if (["www.youtube.com"].includes(domain)) {
+  if (["www.youtube.com", "youtube.com"].includes(domain)) {
     return "youtube";
+  }
+  if (["jsfiddle.net"].includes(domain)) {
+    return "jsfiddle";
+  }
+  if (["codesandbox.io"].includes(domain)) {
+    return "codesandbox";
+  }
+  if (["stackblitz.com"].includes(domain)) {
+    return "stackblitz";
+  }
+  if (["blueprintue.com"].includes(domain)) {
+    return "blueprintue";
   }
   return null;
 };
